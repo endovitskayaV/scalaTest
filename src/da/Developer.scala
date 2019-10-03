24a02737d162e809d07d7a027c6ba9b47738a26f
Valentina Endovitskaya <Valentina.Endovitskaya@dataart.com>
2019-10-03 16:25:36 +0300

    loops and break feature

--- src/da/Developer.scala
+++
@@ -1,7 +0,0 @@
-package da
-
-class Developer(val name: String, val specialty: String) {
-  def writeCode() {
-    println(this.specialty + " writes code.");
-  }
-}
--- src/da/VariablesDemo.scala
+++ src/da/VariablesDemo.scala
@@ -1,10 +1,21 @@
 package da
 
+import scala.util.control.Breaks
+
 object VariablesDemo {
   def main(args: Array[String]) {
-    val javaScalaDeveloper = new Developer("Eugene Suleimanov", "Java/Scala Developer")
-    println("Developer name: " + javaScalaDeveloper.name)
-    println("Developer specialty: " + javaScalaDeveloper.specialty)
-    javaScalaDeveloper.writeCode()
+    val integerList = List(10, 20, 30, 40, 50);
+
+    val filteredValue = for {counter <- integerList if counter != 10; if counter < 50}
+      yield counter
+
+    val loop = new Breaks;
+    loop.breakable({
+      for (counter <- filteredValue) {
+        println(counter)
+        loop.break()
+      }
+    }
+    )
   }
 }
