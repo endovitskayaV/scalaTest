e5cc8240a5fa9b5daff7e6aa1e3bbe6ba258f95f
Valentina Endovitskaya <Valentina.Endovitskaya@dataart.com>
2019-10-03 17:50:20 +0300

    exceptions feature

--- src/da/VariablesDemo.scala
+++ src/da/VariablesDemo.scala
@@ -1,25 +1,16 @@
 package da
 
-import scala.util.control.Breaks
-import Array._
-
 object VariablesDemo {
   def main(args: Array[String]) {
-    val simpleTuple = ("String", 100500, "FFFFF")
-
-    println("simpleTuple: ")
-    simpleTuple.productIterator.foreach { element => println("Element: " + element) }
-
-    val integerTuple = ("1", 2, 3,"rt")
-
-    val integerTupleSum = integerTuple._1 + integerTuple._2 + integerTuple._3 + integerTuple._4
-
-    println("integerTuple: " + integerTuple)
-    println("integerTupleSum: " + integerTupleSum)
-
-    val stringTuple = Tuple2("First", "Second")
-
-    println("stringTuple: " + stringTuple)
-    println("Swapped integerTuple: " + stringTuple.swap)
+    try {
+      val result = 100 / 0
+      println(result)
+    } catch {
+      case ex: ArithmeticException => {
+        println("ArithmeticException - cannot divide by zero")
+      }
+    }finally {
+      println("Block finally. This message will be shown in most cases.")
+    }
   }
 }
