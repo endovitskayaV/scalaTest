2f68e3bafcc8ed178d400de62e5009b321afe891
Valentina Endovitskaya <Valentina.Endovitskaya@dataart.com>
2019-10-03 17:36:55 +0300

    set feature

--- src/da/VariablesDemo.scala
+++ src/da/VariablesDemo.scala
@@ -5,27 +5,17 @@ import Array._
 
 object VariablesDemo {
   def main(args: Array[String]) {
-    var users = Map(1 -> "Ivan Ivanov", 2 -> "Kolya Nikolaev", 3 -> "Viktor Viktorov")
-    var users2 = Map()
+    val skills1 = Set("Java", "Java", "SQL", "SQL")
+    val skills2 = Set("Scala", "Scala", "Play", "Play")
 
-    println("users: " + users)
-    println("users keys: " + users.keys)
-    println("users values: " + users.values)
+    println("skills1: " + skills1)
+    println("skills2: " + skills2)
 
-    println("users2: " + users2)
-    println("users2 keys: " + users2.keys)
-    println("users2 values: " + users2.values)
+    val allSkills = skills1 ++ skills2
 
+    println("allSkills: " + allSkills)
 
-    val moreUsers = Map(4 -> "Innokentii Morozov", 5 -> "Mikhail Mikheev")
-
-    println("moreUsers: " + moreUsers)
-
-    users = users ++ moreUsers
-
-    println("users: " + users)
-
-    println("users contains key 1: " + users.contains(1))
-    println("users contains key 100: " + users.contains(100))
+    println("Max element allSkills: " + allSkills.max)
+    println("Min element allSkills: " + allSkills.min)
   }
 }
