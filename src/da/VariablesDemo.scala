70e5807b871510b306d1651b20f969d8bc21d1b0
Valentina Endovitskaya <Valentina.Endovitskaya@dataart.com>
2019-10-03 17:01:09 +0300

    closure feature

--- src/da/VariablesDemo.scala
+++ src/da/VariablesDemo.scala
@@ -5,21 +5,10 @@ import Array._
 
 object VariablesDemo {
   def main(args: Array[String]) {
-    val integerArray = Array(1, 2)
-    val integerArray1 = new  Array[String](10)
-    println("integerArray: ")
-    for (arrayElement <- integerArray) {
-      print(arrayElement + " ")
-    }
-//concat(integerArray, integerArray1,integerArray)
-    println()
-
-    var integerArrayTotalSum = 0
-
-    for (arrayElement <- integerArray) {
-      integerArrayTotalSum += arrayElement
-    }
-
-    println("Total sum of array elements is: " + integerArrayTotalSum)
+    println("devider(300): " + devider(301))
+    println("devider(600): " + devider(600))
   }
+
+  var deviderFactor = 2
+  val devider = (i: Int) => i % deviderFactor
 }
