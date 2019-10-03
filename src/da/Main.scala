f60cdb95d31f1f2bbe51f4cb4177505e72752e30
Valentina Endovitskaya <Valentina.Endovitskaya@dataart.com>
2019-10-03 18:36:46 +0300

    regexp feature

--- src/da/Main.scala
+++ src/da/Main.scala
@@ -1,28 +1,14 @@
 package da
 
-trait BaseTrait extends BaseTrait2 with BaseTrait3 {
-  def isScalaDeveloper(x: String): Boolean = x.equals("Scala Developer")
-}
-
-trait BaseTrait2 {
-  def isScalaDeveloper2(x: String): Boolean = x.equals("Scala Developer")
-}
-
-trait BaseTrait3 {
-  def isScalaDeveloper3(x: String): Boolean = x.equals("Scala Developer")
-}
-
-class Developer(sSpecialty: String) extends BaseTrait with BaseTrait2 with BaseTrait3 {
-  var specialty: String = sSpecialty
-}
-
 
 object Main {
   def main(args: Array[String]) {
-    val scalaDeveloper = new Developer("Scala Developer")
-    val javaDeveloper = new Developer("Java Developer")
+    var pattern = "Scala Developer".r
+    var scalaDeveloper = "Eugene Suleimanov is a Java/Scala Developer"
+    println(pattern findFirstIn scalaDeveloper)
 
-    println(scalaDeveloper.isScalaDeveloper(scalaDeveloper.specialty))
-    println(javaDeveloper.isScalaDeveloper3(javaDeveloper.specialty))
+    pattern = "C".r
+    scalaDeveloper = "Eugene Suleimanov is C developer"
+    println(pattern replaceAllIn(scalaDeveloper, "Java/Scala"))
   }
 }
