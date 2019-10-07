db96d6a1feec1a32dc4fa10f5f8acb8d8b6ad803
Valentina Endovitskaya <Valentina.Endovitskaya@dataart.com>
2019-10-07 15:18:09 +0300

    operators feature

--- src/da/Main.scala
+++ src/da/Main.scala
@@ -2,29 +2,17 @@ package da
 
 
 object Main {
-  abstract class Monoid[A] {
-    def add(x: A, y: A): A
-    def unit: A
+  def main(args: Array[String]): Unit = {
+    var t= T(3)
+    var r=T(4)
+    print(t f r);
   }
 
-  object ImplicitTest {
-    implicit val stringMonoid: Monoid[String] = new Monoid[String] {
-      def add(x: String, y: String): String = x concat y
-      def unit: String = ""
-    }
 
-    implicit val intMonoid: Monoid[Int] = new Monoid[Int] {
-      def add(x: Int, y: Int): Int = x + y
-      def unit: Int = 0
-    }
-
-    def sum[A](xs: List[A])(implicit m: Monoid[A]): A =
-      if (xs.isEmpty) m.unit
-      else m.add(xs.head, sum(xs.tail))
-
-    def main(args: Array[String]): Unit = {
-      println(sum(List(1, 2, 3)))       // использует intMonoid неявно
-      println(sum(List("a", "b", "c"))) // использует stringMonoid неявно
-    }
   }
-  }
+case class T(u:Int) {
+
+  def f(x: T) = x.u +this.u
+
+
+}
